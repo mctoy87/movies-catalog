64b54e3af19599a02d822d1ceac201cd8f5a7875
mctoy87 <mctoy87@mail.ru>
2025-01-17 16:08:14 +0200

    added lodash npm and create films array in backend

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,18 +1,28 @@
 import {initTRPC} from '@trpc/server';
+import _ from 'lodash';
 
-const films = [
-  {id: 1234, nameRu: 'Man in black', year: 2001, length: 189, rating: 8.2},
-  {id: 1235, nameRu: 'Oblivion', year: 2001, length: 189, rating: 8.2},
-  {
-    id: 1236,
-    nameRu: 'Back to the future',
-    year: 2001,
-    length: 189,
-    rating: 8.2,
-  },
-  {id: 1237, nameRu: 'Forest Gump', year: 2001, length: 189, rating: 8.2},
-  {id: 1238, nameRu: 'Home alone', year: 2001, length: 189, rating: 8.2},
-];
+const films = _.times(100, (i) => ({
+  id: `${i}`,
+  nameRu: `Film title id: ${i}`,
+  year: `${1900 + i}`,
+  length: 189,
+  rating: 8.2,
+  description: `Описание фильма id: ${i}`,
+}));
+
+// const films = [
+//   {id: 1234, nameRu: 'Man in black', year: 2001, length: 189, rating: 8.2},
+//   {id: 1235, nameRu: 'Oblivion', year: 2001, length: 189, rating: 8.2},
+//   {
+//     id: 1236,
+//     nameRu: 'Back to the future',
+//     year: 2001,
+//     length: 189,
+//     rating: 8.2,
+//   },
+//   {id: 1237, nameRu: 'Forest Gump', year: 2001, length: 189, rating: 8.2},
+//   {id: 1238, nameRu: 'Home alone', year: 2001, length: 189, rating: 8.2},
+// ];
 
 const x: string = 'hello';
 
@@ -28,7 +38,11 @@ const trpc = initTRPC.create();
  * that can be used throughout the router
  */
 export const trpcRouter = trpc.router({
-  getfilms: trpc.procedure.query(() => ({films})),
+  getfilms: trpc.procedure.query(() => ({
+    films: films.map((film) =>
+      _.pick(film, ['id', 'nameRu', 'year', 'rating'])
+    ),
+  })),
 });
 
 export type TrpcRouter = typeof trpcRouter;
